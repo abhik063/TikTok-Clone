58792bbd56300c86c7bda4eccd4cbdd718613b64
Abhishek Kumar <kumarabhishek063@outlook.com>
2020-09-14 11:54:29 +0530

    Front End Done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import "./App.css";
+import Video from "./Video";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="app_videos">
+        <Video
+          url="https://v16m.tiktokcdn.com/77b0b8ba4906985560015c3428cc56f8/5f58ca64/video/tos/useast2a/tos-useast2a-ve-0068c002/4ffdaa38f94145bea9a266c53c521cd1/?a=1233&amp;br=1614&amp;bt=807&amp;cr=0&amp;cs=0&amp;cv=1&amp;dr=0&amp;ds=3&amp;er=&amp;l=202009090627550101890740365B0617F9&amp;lr=tiktok_m&amp;mime_type=video_mp4&amp;qs=0&amp;rc=aml4ajV1c2RtdzMzaDczM0ApaGY3aTQ5OGQ5NzpnPGRnZWdyMy0tcDVkay9fLS0xMTZzc19jMzUuYS02YTBiNi9fYmM6Yw%3D%3D&amp;vl=&amp;vr="
+          channel="duaLipa"
+          description="it works"
+          songs="i feel good"
+          messages={456}
+          likes={600}
+          shares={789}
+        />
+        <Video
+          url="https://v77.tiktokcdn.com/312e8a95348d9aea5cd22d78fbf3bf82/5f58cc25/video/tos/useast2a/tos-useast2a-pve-0068/352d7e3aa0834f5bb37eb094da5a8886/?a=1233&amp;br=3164&amp;bt=1582&amp;cr=0&amp;cs=0&amp;cv=1&amp;dr=0&amp;ds=3&amp;er=&amp;l=202009090635340101901851300506715C&amp;lr=tiktok_m&amp;mime_type=video_mp4&amp;qs=0&amp;rc=Mzllc3FnM3lydzMzPDczM0ApaWY2Mzw3OmVpN2lkNDNlZmdfajBpaGZwcTFfLS0vMTZzczBgMF4zYDA0MmIwY2FiYTI6Yw%3D%3D&amp;vl=&amp;vr="
+          channel="duaLipa"
+          description="it works"
+          songs="i feel good"
+          messages={456}
+          likes={600}
+          shares={789}
+        />
+      </div>
     </div>
   );
 }
---
+++ src/VideoSidebar.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import "./VideoSidebar.css";
+import FavouriteIcon from "@material-ui/icons/Favorite";
+import FavouriteBorderIcon from "@material-ui/icons/FavoriteBorder";
+import MessageIcon from "@material-ui/icons/Message";
+import ShareIcon from "@material-ui/icons/Share";
+function VideoSidebar({ likes, shares, messages }) {
+  const [liked, setLiked] = useState(false);
+  return (
+    <div className="videoSidebar">
+      <div className="videoSidebar_button">
+        {liked ? (
+          <FavouriteIcon fontSize="large" onClick={(e) => setLiked(true)} />
+        ) : (
+          <FavouriteBorderIcon
+            fontSize="large"
+            onClick={(e) => setLiked(true)}
+          />
+        )}
+
+        <p>{liked ? likes + 1 : likes}</p>
+      </div>
+      <div className="videoSidebar_button">
+        <MessageIcon fontSize="large" />
+        <p>{liked ? messages + 1 : messages}</p>
+      </div>
+      <div className="videoSidebar_button">
+        <ShareIcon fontSize="large" />
+        <p>100</p>
+      </div>
+    </div>
+  );
+}
+
+export default VideoSidebar;
